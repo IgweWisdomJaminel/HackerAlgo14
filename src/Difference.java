f0fbb6084c852434bf37f9ae88486db3413c10da
jaminel <jamineltechnology@gmail.com>
2024-05-10 11:40:05 +0100

    update

---
+++ src/Difference.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Difference {
+    int input;
+
+    int N;
+
+    int[]arrayDiffrence= new int[N];
+    int maximumDifference;
+
+    public void computerDifference(){
+        Scanner sc= new Scanner(System.in);
+        for(int i=0; i<arrayDiffrence.length; i++){
+
+        }
+        for(int N:arrayDiffrence){
+            System.out.println(N);
+        }
+
+
+
+    }
+    public Difference(int[]arrayDiffrence){
+
+        this.arrayDiffrence=arrayDiffrence;
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+    }
+}
