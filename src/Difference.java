cd87c0476ce50e34e845fe9d233e4902571d19ee
jaminel <jamineltechnology@gmail.com>
2024-05-11 06:35:10 +0100

    update

--- src/Difference.java
+++ src/Difference.java
@@ -9,13 +9,40 @@ public class Difference {
     int maximumDifference;
 
     public void computerDifference(){
-        Scanner sc= new Scanner(System.in);
-        for(int i=0; i<arrayDiffrence.length; i++){
+
+        int highestNumber=0;
+        int lowesNumber=0;
+        int firstElement=arrayDiffrence[0];
+        int firstElement2=arrayDiffrence[0];
+        int count=0;
+        for(int i:arrayDiffrence){
+            if(firstElement2<=i){
+                firstElement2=i;
+               highestNumber=firstElement2;
+            }
+            if(firstElement>=i){
+               firstElement=i;
+               lowesNumber=firstElement;
+            }
+
+
 
         }
-        for(int N:arrayDiffrence){
-            System.out.println(N);
+        if(highestNumber==lowesNumber){
+            maximumDifference=0;
         }
+        System.out.println(highestNumber);
+        System.out.println(lowesNumber);
+        maximumDifference= highestNumber-lowesNumber;
+        System.out.println(maximumDifference);
+
+
+
+
+
+
+
+
 
 
 
@@ -29,7 +56,13 @@ public class Difference {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int N= sc.nextInt();
+        int[]array= new int[N];
+        for(int i=0; i<array.length; i++){
+            array[i]=sc.nextInt();
+        }
+        Difference difference = new Difference(array);
+        difference.computerDifference();
 
 
     }
